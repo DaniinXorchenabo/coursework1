3ec8dfebf83159ae48389d1cf9b09d276c6d128a
DaniinXorchenabo <rkbcu@mail.ru>
2020-10-09 21:54:06 +0300

    create point class

---
+++ app/point.cpp
@@ -0,0 +1,21 @@
+
+class Point
+{
+    public:
+        float x, y;
+        //=======! Конструкторы !=======
+        Point();
+
+        Point(int x_start, int y_start);
+
+        void muve_up(int x_serup, int y_serup);
+
+        int get_x();
+
+        int get_y();
+
+
+    private:
+
+
+};
