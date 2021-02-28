1d83cd72979b0b43eef65836831c9c3bde0900cc
DaniinXorchenabo <rkbcu@mail.ru>
2021-02-28 12:47:55 +0300

    конструктор базовой фигуры, в который передаются точки теперь работает, все списки используют unique_ptr

--- app2/BaseFigure1_test.cpp
+++ app2/BaseFigure1_test.cpp
@@ -1,6 +1,11 @@
 #include <list>
+#include <iterator>
+#include <memory>
 #include "python_graphics.h"
-using std::list;
+using namespace std;
+//using std::list;
+//using std::iterator;
+//using std::unique_ptr;
 
 class Point{
 
@@ -53,53 +58,58 @@ class Point{
 
 };
 
-//class BaseFigure{
-//
-//    private:
-//        int canvas;
-//
-//    public:
-//        list<BaseFigure&> complex_figure = list<BaseFigure*>();
-//        list<Point> points_with_line = list<Point>(); // точки, соеденнные линией
-//        list<Point> points_no_line = list<Point>();  // точки, не соеденённые линией
-//
-//        bool is_complex_figure(){
-//            return complex_figure != NULL;
-//        }
-//        int get_canvas(){
-//            return canvas;
-//        }
-//
-//        BaseFigure(){}
-//
-//
-//        BaseFigure(const initializer_list<Point> &points)
+class BaseFigure{
+
+    private:
+        int canvas;
+
+    public:
+        list<unique_ptr<BaseFigure>> complex_figure = {};
+        list<unique_ptr<Point>> points_with_line = {}; // точки, соеденнные линией
+        list<unique_ptr<Point>> points_no_line = {};  // точки, не соеденённые линией
+
+        bool is_complex_figure(){
+            return false;
+        }
+
+        int get_canvas(){
+            return canvas;
+        }
+
+        BaseFigure(){}
+
+
+//        BaseFigure(int my_canvas, const initializer_list<Point> &points)
 //        {
-//            points_with_line(points){};
-//        }
-//
-//        BaseFigure(int my_canvas, const initializer_list<Point> &points){
 //            /* Вызов BaseFigure(my_canvas, { p1, p2, p3 }) */
-//
 //            canvas = my_canvas;
 //            points_with_line(points){};
-////            for (auto p = &points; *p; p++){ points_with_line.add(*p); }
 //        }
-//
+
+        BaseFigure(int my_canvas, Point points, ...){
+            /* Вызов BaseFigure(my_canvas, { p1, p2, p3 }) */
+
+            canvas = my_canvas;
+            for (Point* p = &points; p; p++){
+
+                points_with_line.push_back(make_unique<Point>(*p));
+            }
+        }
+
 //        BaseFigure(int my_canvas, int coordinates, ...){
 //            canvas = my_canvas;
 //            for (auto coordinate = &coordinates; *coordinate; coordinate++){
 //                points_with_line.add(canvas, Point(coordinate, ++coordinate));
 //            }
 //        }
-//
+
 //        BaseFigure(BaseFigure figure_material){
 //            canvas = figure_material.get_canvas();
 //            points_no_line = figure_material.points_no_line;
 //            points_with_line = figure_material.points_with_line;
 //            complex_figure = figure_material.complex_figure;
 //        }
-//
+
 //        void draw(){
 //            if (len(complex_figure) > 1){
 //                for (auto figure: complex_figure){
@@ -119,7 +129,7 @@ class Point{
 //                }
 //            }
 //        }
-//
+
 //        const BaseFigure operator+=(const BaseFigure& other_figure) const {
 //            if ((len(complex_figure) == 0) && (len(other_figure.complex_figure) == 0)){
 //                complex_figure.add(BaseFigure(this));
@@ -141,12 +151,12 @@ class Point{
 //
 //            }
 //            else if ((len(complex_figure) != 0) && (len(other_figure.complex_figure) != 0)){
-//                complex_figure.exists(other_figure.complex_figure);
+//                complex_figure.merge(other_figure.complex_figure);
 //            }
 //            return this
 //        }
-//
-//};
+
+};
 
 
 int main(){
