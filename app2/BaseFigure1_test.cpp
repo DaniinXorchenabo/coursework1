45e81128143fa5e3ee7c9c4972dd0eb1d958b3b0
DaniinXorchenabo <rkbcu@mail.ru>
2021-03-03 14:14:21 +0300

    Код запускается и работает без ошибок. + и += в базовом классе работают, чуть-чуть подправлен +, обавлено много лишних выводов, переписан конструктор для BaseFigure ля принятия точек, поправлен порядок вывода линии

--- app2/BaseFigure1_test.cpp
+++ app2/BaseFigure1_test.cpp
@@ -1,11 +1,32 @@
 #include <list>
 #include <iterator>
 #include <memory>
-#include "python_graphics.h"
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+//#include "python_graphics.h"
+
+typedef void (*VoidReturnFunc)();
+typedef int (*intReturnFunc)();
+typedef int (*intReturnFunc1int)(int);
+typedef int (*intReturnFunc2int)(int, int);
+typedef void (*VoidReturnFunc3int)(int, int, int);
+typedef void (*VoidReturnFunc5int)(int, int, int, int, int);
+
+   HINSTANCE a = LoadLibrary("python_graphics.dll");
+   VoidReturnFunc myFunc;
+//   intReturnFunc1int init_console, print_class;
+//   VoidReturnFunc3int draw_point_python;
+//   VoidReturnFunc5int draw_line_python;
+
+   intReturnFunc1int init_console = (intReturnFunc1int) GetProcAddress(a, "create_obj");
+   intReturnFunc1int print_class = (intReturnFunc1int) GetProcAddress(a, "print_class");
+   VoidReturnFunc3int draw_point_python = (VoidReturnFunc3int) GetProcAddress(a, "draw_point_python");
+   VoidReturnFunc5int draw_line_python =  (VoidReturnFunc5int) GetProcAddress(a, "draw_line_python");
+
 using namespace std;
-//using std::list;
-//using std::iterator;
-//using std::unique_ptr;
+
+
 
 class Point{
 
@@ -50,9 +71,22 @@ class Point{
             y = copy.get_raw_y();
         }
         void draw(){
-            draw_point_python(canvas, x, y);
+            cout<<"Draw point";
+            cout<<get_x();
+            cout<<", ";
+            cout<<get_y()<<endl;
+//            draw_point_python(canvas, x, y);
         }
         void draw_line(Point& point){
+//            cout<<"Draw line from (";
+//            cout<<get_x();
+//            cout<<", ";
+//            cout<<get_y();
+//            cout<<") to (";
+//            cout<<point.get_x();
+//            cout<<", ";
+//            cout<<point.get_y();
+//            cout<<")\n";
             draw_line_python(canvas, get_x(), get_y(), point.get_x(), point.get_y());
         }
 
@@ -80,14 +114,29 @@ class BaseFigure{
 
         BaseFigure(int my_canvas){ canvas = my_canvas; }
 
-        BaseFigure(int my_canvas, Point points, ...){
+        BaseFigure(int my_canvas, initializer_list<Point> points){
             /* Вызов BaseFigure(my_canvas, p1, p2, p3}) */
-
             canvas = my_canvas;
-            for (Point* p = &points; p; p++){
-                points_with_line.push_back(make_shared<Point>(*p));
+            for (auto iter : points){
+                points_with_line.push_back(make_shared<Point>(iter));
             }
         }
+
+       /* BaseFigure(int my_canvas, Point points, ...){
+            // Вызов BaseFigure(my_canvas, p1, p2, p3})
+               cout<<"**********----"<<endl;
+
+            canvas = my_canvas;
+            cout<<"**********----"<<endl;
+            for (Point* p = &points; points>0; points--){
+                cout<<"**********----1"<<endl;
+                points_with_line.push_back(make_shared<Point>(*(++p)));
+                cout<<"**********----2"<<endl;
+            }
+            cout<<"**********----"<<endl;
+        }*/
+//        g++ BaseFigure1_test.cpp -o main.exe
+//
 
         BaseFigure(int my_canvas, int coordinates, ...){
             canvas = my_canvas;
@@ -112,7 +161,7 @@ class BaseFigure{
             else {
                 auto first_point = points_with_line.back();
                 for (auto second_point: points_with_line){
-                    second_point->draw_line(*first_point);
+                    first_point->draw_line(*second_point);
                     first_point = second_point;
                 }
                 for (auto point: points_no_line){
@@ -149,7 +198,7 @@ class BaseFigure{
             return *this;
         }
 
-        const BaseFigure operator+(const BaseFigure& right) {
+        BaseFigure operator+(const BaseFigure& right) {
 
             BaseFigure new_figure(get_canvas());
 
@@ -181,6 +230,22 @@ class BaseFigure{
 
 
 int main(){
-//    BF({Point(1,2), Point(1,2), Point(1,2), Point(1,2)})
+
+
+   int my_obj = init_console(5694);
+   print_class(my_obj);
+    BaseFigure one(1, { Point(1,1,1), Point(1,1,2),Point(1,1,3),Point(1,1,4),Point(1,1,5) });
+    BaseFigure one_1(1, { Point(1,2,1), Point(1,2,2),Point(1,2,3),Point(1,2,4),Point(1,2,5) });
+    BaseFigure one_3(1, { Point(1,3,1), Point(1,3,2),Point(1,3,3),Point(1,3,4),Point(1,3,5) });
+
+    BaseFigure one_4 = one + one_1 + one_3;
+    one_4.draw();
+    cout<<"0**********----!!"<<endl;
+    one.draw();
+    cout<<"1**********----!!"<<endl;
+    one_1.draw();
+    cout<<"2**********----!!"<<endl;
+    one_3.draw();
+    getch();
     return 0;
 }
