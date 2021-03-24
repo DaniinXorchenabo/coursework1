2495f33a31edac7bb3add7acf396cd66e154357a
DaniinXorchenabo <rkbcu@mail.ru>
2021-03-24 14:51:10 +0300

    change colors line added in lines list

--- create_dll_lib/test_dll.py
+++ create_dll_lib/test_dll.py
@@ -6,11 +6,15 @@
 from itertools import cycle
 from time import sleep
 import copy
+import sys
 from random import randint, random
 
-from asciimatics.screen import Screen
 from asciimatics.effects import Print
-from asciimatics.renderers import FigletText, Rainbow, DynamicRenderer
+from asciimatics.renderers import DynamicRenderer
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+from asciimatics.exceptions import ResizeScreenError
+
 
 screen = Screen.open(None,
                      catch_interrupt=False,
@@ -136,7 +140,8 @@ def my_new_draw(self, x, y, char=None, colour=cycle([7]), bg=0, thin=False):
             _draw_on_y(x0 + 1, y0)
 
 
-all_lines = [(10, 20, 50, 60)]  # (x0, y0, x1, y1)
+all_lines = [(10, 20, 30, 10), (30, 10, 50, 40, cycle(range(4, 8))), (50, 40, -20, 10)]  # (x0, y0, x1, y1)
+
 
 class MyNiceLire(DynamicRenderer):
     """
@@ -190,7 +195,7 @@ class MyNiceLire(DynamicRenderer):
     _line_chars = " ''^.|/7.\\|Ywbd#"
     _uni_line_chars = " ▘▝▀▖▌▞▛▗▚▐▜▄▙▟█"
 
-    def __init__(self, height, width, emitter, intensity, spot, colours,
+    def __init__(self, height, width, colours,
                  bg=False, unicode_aware=False):
         """
         :param height: Height of the box to contain the flames.
@@ -205,16 +210,17 @@ class MyNiceLire(DynamicRenderer):
         """
         super(MyNiceLire, self).__init__(height, width)
         self._unicode_aware = unicode_aware
-        self._emitter = emitter
-        self._intensity = intensity
-        self._spot_heat = spot
-        self._count = len([c for c in emitter if c not in " \n"])
+        # self._emitter = emitter
+        # self._intensity = intensity
+        # self._spot_heat = spot
+        # self._count = len([c for c in emitter if c not in " \n"])
         line = [0 for _ in range(self._width)]
         char_line = ["" for _ in range(self._width)]
         self._buffer = [copy.deepcopy(line) for _ in range(self._width * 2)]
         self._char_buffer = [copy.deepcopy(char_line) for _ in range(self._width * 2)]
         self._colours = self._COLOURS_256 if colours >= 256 else \
             self._COLOURS_16
+        self._colors_console_count = colours
         self._bg_too = bg
 
         # Figure out offset of emitter to centre at the bottom of the buffer
@@ -225,9 +231,7 @@ class MyNiceLire(DynamicRenderer):
             e_height += 1
         self._x = (width - e_width) // 2
         self._y = height - e_height
-        self.line_chars = (self._uni_line_chars if self._unicode_aware else
-                      self._line_chars)
-
+        self.line_chars = (self._uni_line_chars if self._unicode_aware else self._line_chars)
 
     def get_from(self, x, y):
         """
@@ -249,37 +253,18 @@ class MyNiceLire(DynamicRenderer):
         return None
 
     def _render_now(self, char=None, thin=False):
-        # First make the fire rise with convection
-        # Поднятие огня вверх
-        # for y in range(len(self._buffer) - 1):
-        #     self._buffer[y] = self._buffer[y + 1]
-        # self._buffer[len(self._buffer) - 1] = [0 for _ in range(self._width)]
 
-        # Seed new hot spots
-        # x = self._x
-        # y = self._y
-        # for c in self._emitter:
-        #     if c not in " \n" and random() < self._intensity:
-        #         self._buffer[y][x] += randint(1, self._spot_heat)
-        #     if c == "\n":
-        #         x = self._x
-        #         y += 1
-        #     else:
-        #         x += 1
-
-        # Seed a few cooler spots
-        # for _ in range(self._width // 2):
-        #     self._buffer[randint(0, self._height - 1)][
-        #         randint(0, self._width - 1)] -= 10
-
-        # Define line end points.
-
+        color_iterator = cycle(range(16))
         char_line = ["" for _ in range(self._width)]
         self._char_buffer = [copy.deepcopy(char_line) for _ in range(self._width * 2)]
 
-        for (x0, y0, x1, y1) in all_lines:
+        for (x0, y0, x1, y1, *new_color_iter) in all_lines:
             x0, y0 = int(round(x0 * 2, 0)), int(round(y0 * 2, 0))
             x1, y1 = int(round(x1 * 2, 0)), int(round(y1 * 2, 0))
+            if new_color_iter and bool(new_color_iter):
+                color_iterator = new_color_iter[0]
+                if len(new_color_iter) > 1:
+                    self._colours = new_color_iter[1]
 
             # Don't bother drawing anything if we're guaranteed to be off-screen
             if ((x0 < 0 and x1 < 0) or (x0 >= self._width * 2 and x1 >= self._width * 2) or
@@ -302,7 +287,7 @@ class MyNiceLire(DynamicRenderer):
 
             def _fast_fill(start_x, end_x, iy):
                 next_char = -1
-                color_iterator = cycle(range(16))
+
                 for ix in range(start_x, end_x):
                     if ix % 2 == 0 or next_char == -1:
                         next_char = _get_start_char(ix // 2, iy // 2)
@@ -310,18 +295,15 @@ class MyNiceLire(DynamicRenderer):
                     if ix % 2 == 1:
                         self._char_buffer[iy // 2][ix // 2] = self.line_chars[next_char]
                         self._buffer[iy // 2][ix // 2] = next(color_iterator)
-                        # self.print_at(self.line_chars[next_char], ix // 2, iy // 2, next(colour), bg=bg)
                 if end_x % 2 == 1:
                     self._char_buffer[iy // 2][end_x // 2] = self.line_chars[next_char]
                     self._buffer[iy // 2][end_x // 2] = next(color_iterator)
-                    # self.print_at(self.line_chars[next_char], end_x // 2, iy // 2, next(colour), bg=bg)
 
             def _draw_on_x(ix, iy):
                 err = dx
                 px = ix - 2
                 py = iy - 2
                 next_char = 0
-                color_iterator = cycle(range(16))
                 while ix != x1:
                     if ix < px or ix - px >= 2 or iy < py or iy - py >= 2:
                         px = ix & ~1
@@ -335,11 +317,9 @@ class MyNiceLire(DynamicRenderer):
                     ix += sx
 
                     if char is None:
-                        # self.print_at(self.line_chars[next_char], px // 2, py // 2, next(colour), bg=bg)
                         self._char_buffer[py // 2][px // 2] = self.line_chars[next_char]
                         self._buffer[py // 2][px // 2] = next(color_iterator)
                     else:
-                        # self.print_at(char, px // 2, py // 2, next(colour), bg=bg)
                         self._char_buffer[py // 2][px // 2] = char
                         self._buffer[py // 2][px // 2] = next(color_iterator)
 
@@ -348,7 +328,6 @@ class MyNiceLire(DynamicRenderer):
                 px = ix - 2
                 py = iy - 2
                 next_char = 0
-                color_iterator = cycle(range(16))
                 while iy != y1:
                     if ix < px or ix - px >= 2 or iy < py or iy - py >= 2:
                         px = ix & ~1
@@ -362,8 +341,6 @@ class MyNiceLire(DynamicRenderer):
                     iy += sy
 
                     if char is None:
-                        # self.print_at(self.line_chars[next_char],
-                        #               px // 2, py // 2, next(colour), bg=bg)
                         self._buffer[py // 2][px // 2] = next(color_iterator)
                         self._char_buffer[py // 2][px // 2] = self.line_chars[next_char]
                     else:
@@ -382,20 +359,6 @@ class MyNiceLire(DynamicRenderer):
                 if not thin:
                     _draw_on_y(x0 + 1, y0)
 
-
-        # Simulate cooling effect of the resulting environment.
-        # for y in range(len(self._buffer)):
-        #     for x in range(self._width):
-        #         new_val = self._buffer[y][x]
-        #         if y < len(self._buffer) - 1:
-        #             new_val += self._buffer[y + 1][x]
-        #             if x > 0:
-        #                 new_val += self._buffer[y][x - 1]
-        #             if x < self._width - 1:
-        #                 new_val += self._buffer[y][x + 1]
-        #         self._buffer[y][x] = new_val // 4
-
-        # Now build the rendered text from the simulated flames.
         self._clear()
         for x in range(self._width):
             for y in range(len(self._buffer)):
@@ -406,30 +369,13 @@ class MyNiceLire(DynamicRenderer):
                         char = " "
                         bg = colour[0]
                     else:
-                        # char = self._CHARS[min(len(self._CHARS) - 1,
-                        #                    self._buffer[y][x])]
                         char = self._char_buffer[y][x]
                         bg = 0
                     self._write(char, x, y, colour[0], colour[1], bg)
-
         return self._plain_image, self._colour_map
 
 
 setattr(Screen, "my_new_draw", my_new_draw)
-
-# screen.move(0, 0)
-# screen.my_new_draw(50, 30, colour=cycle(range(0, 8)))
-# # screen.print_at("#", 55, 35, 196, 2, 1, False)
-# screen.refresh()
-# sleep(5)
-
-from asciimatics.renderers import FigletText, Fire
-from asciimatics.scene import Scene
-from asciimatics.screen import Screen
-from asciimatics.effects import Print
-from asciimatics.exceptions import ResizeScreenError
-from pyfiglet import Figlet
-import sys
 
 
 def demo(screen):
@@ -437,8 +383,7 @@ def demo(screen):
 
     effects = [
         Print(screen,
-              MyNiceLire(screen.height, 80, "*" * 70, 0.8, 60, screen.colours,
-                   bg=screen.colours >= 256),
+              MyNiceLire(screen.height, screen.width, screen.colours, bg=screen.colours >= 256),
               0,
               speed=1,
               transparent=False),
@@ -448,6 +393,7 @@ def demo(screen):
 
 
 if __name__ == "__main__":
+
     while True:
         try:
             Screen.wrapper(demo)
