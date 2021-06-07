a676429972a4a01fcedc09225c5e90ed66e115b4
DaniinXorchenabo <rkbcu@mail.ru>
2021-06-07 17:50:14 +0300

    refactoring project structure

--- app/main.cpp
+++ app/main.cpp
@@ -1,111 +1,53 @@
-#include <iostream>
-#define _WIN32_WINNT 0x0500
-#include <windows.h>
-#include <wincon.h>
-using namespace std;
-
-
-long WINAPI mainProc(HWND hWnd,UINT message,WPARAM wParam,LPARAM lParam)
-{
-//Тут вот будем обрабатывать сообщения.
-   switch(message)
-  {
-  case WM_CLOSE:
-         {
-          PostQuitMessage(0);
-         };break;
-  }
-  return 0;
-}
-
-int APIENTRY WinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPSTR     lpCmdLine,
-                     int       nCmdShow)
-{
-	MSG msg;
-	HWND hMain=CreateWindowEx(0,WC_DIALOG,"Hello world",WS_VISIBLE | WS_SYSMENU,CW_USEDEFAULT,CW_USEDEFAULT,200,200,0,0,0,0);
-	SetWindowLong(hMain,DWLP_DLGPROC,(long)mainProc);
-    while(GetMessage(&msg,0,0,0)!=0)
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-	return 0;
-}
-
-/*
-HWND CreateWindowEx(      
-    DWORD dwExStyle,
-    LPCTSTR lpClassName,
-    LPCTSTR lpWindowName,
-    DWORD dwStyle,
-    int x,
-    int y,
-    int nWidth,
-    int nHeight,
-    HWND hWndParent,
-    HMENU hMenu, 
-    HINSTANCE hInstance,
-    LPVOID lpParam   
-);
-BOOL GetMessage(
-    LPMSG lpMsg,
-    HWND hWnd,
-    UINT wMsgFilterMin,
-    UINT wMsgFilterMax
-);
-
-int main()
-{
-    // MessageBox(0, "Hello ^^^ world", "WinAPI is simple",0);
-    // HWND hMain=CreateWindowEx(0,WC_DIALOG,"Hello world",WS_VISIBLE | WS_SYSMENU, 500,500,200,200,0,0,0,0);
-    // WinMain()
-    // cout << "Hello world!" << endl;
-    // готовимся и настравиваемся на рисование
-    HWND hwnd=GetConsoleWindow();
-    HDC hdc=GetDC(hwnd);
-    HPEN p1,p2=CreatePen(PS_SOLID,1,RGB(255,0,0)); // Создаем красное перо толщиной 1, перо  это то чем рисуем контур фигур
-    HBRUSH b1,b2=CreateSolidBrush(RGB(0,255,0)); // Создаем зеленую кисть, кисть это то чем закрашивается фигура
-    p1=(HPEN)SelectObject(hdc,p2);  // Заносим красное перо в контекст рисования
-    b1=(HBRUSH)SelectObject(hdc,b2);    // Заносим кисть в контекст рисования
- 
-    // рисуем: примеры рисования фигур
-    Ellipse(hdc,10,10,100,100);  // рисуем эллипс (он вписан в прямоугольник заданный координатами)
-    MoveToEx(hdc,100,100,NULL); //переносим точку  начала рисования в новое место иначе линия будет рисоваться от элипса.
-    LineTo(hdc,200,200); // рисуем линию
-    Rectangle(hdc,210,210,300,350);// рисуем прямоугольник
-    RoundRect(hdc,210,110,300,180,10,10);// прямоугольник со скруглёнными краями( последние две цифры это размеры эллипса скругляющего края)
-    TextOutA(hdc,100,150,"Heloo text",10); // Пишем строчку текста в заданных координатах (последняя цифра это количество отображаемых букв)
-    SetPixel(hdc,130,120,RGB(0,255,0)); // вот как зажечь пиксел например для рисования функций
-    while(true){}
-    return 0;
-}
-
-*/
-/*
-#include <iostream>
-// #pragma once
-#include "point.h"
-#include "figure.h"
-#include "console_worker.h"
 #include <list>
-#include <vector>
 #include <iterator>
+#include <memory>
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <chrono>
+#include <cmath>
+#include <random>
 
+#include "utils.cpp"
+#include "Point.cpp"
+#include "BaseFigure.cpp"
+#include "headers/import_python_func.h"
+#include "console.cpp"
 
 using namespace std;
 
-int main()
-{
-    Point point1(1, 2);
-    Figure test_figure(list<Point>{Point(1,2)});
-    std::cout << "Hi!" << std::endl;
-    std::cout << test_figure.return_32() << std::endl;
-    std::cout << point1.get_x() << " " << point1.get_y()<< std::endl;
-    ConsoleWorker con1;
-    while (true){
+int main() {
 
-    }
+    int canvas = new_start_python();
+    ConsoleRenderer this_console(canvas);
+//    this_console += BaseFigure(canvas, {
+//        Point(canvas,5,5),
+//        Point(canvas,5,30),
+//        Point(canvas, 30,30),
+//        Point(canvas,30,5)
+//    });
+//    this_console += BaseFigure(canvas, {
+//        Point(canvas,65,10),
+//        Point(canvas,50,10)
+//    });
+//    this_console += BaseFigure(canvas, {
+//        Point(canvas,50,40),
+//        Point(canvas,70,50),
+//        Point(canvas,90,10)
+//    });
+    this_console += BaseFigure(canvas, {
+            Point(canvas, 10, 10),
+            Point(canvas, 30, 10),
+            Point(canvas, 30, 30)
+    });
+    this_console += BaseFigure(canvas, {
+            Point(canvas, 50, 50),
+            Point(canvas, 80, 50),
+            Point(canvas, 80, 80)
+    });
+
+    while (true) { if (this_console.renderer(canvas)) break; }
     return 0;
-}*/
+}
+
+// g++ BaseFigure1_test.cpp -o main.exe -std=c++17
--- app/point.cpp
+++ app/point.cpp
@@ -1,52 +1,112 @@
-#pragma once
-#include "point.h"
+#include <list>
+#include <iterator>
+#include <memory>
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <chrono>
+#include <cmath>
+#include <random>
+#include "headers/import_python_func.h"
+#include "headers/Point.h"
+
+using namespace std;
 
 
-Point::Point(int x_start, int y_start){
-    x = (float)x_start;
-    y = (float)y_start;
+int Point::get_canvas() const {
+    return canvas;
 }
 
-void Point::muve_up(float x_start, float y_start){
-    x += x_start;
-    y += y_start;
+float Point::get_raw_x() const {
+    return x;
 }
 
-void Point::muve_up(int x_serup, int y_serup){
-    x += (float)x_serup;
-    y += (float)y_serup;
+float Point::get_raw_y() const {
+    return y;
 }
 
-int Point::get_x(){
-    return (int)x;
+void Point::reboot_radius() {
+    radius = -1;
 }
 
-int Point::get_y(){
-    return (int)y;
+int Point::get_x() const {
+    return (int) x;
 }
 
-void Point::set_x(int new_x){
-    x = (float)new_x;
+int Point::get_y() const {
+    return (int) y;
 }
 
-void Point::set_y(int new_y){
-    y = (float)new_y;
+bool Point::border_control(int max_x, int max_y) {
+    // Вернет true, если точка находится в пределах экрана
+    return x >= 0 && y >= 0 && x <= max_x && y <= max_y;
 }
 
-void Point::set_point(int new_x, int new_y){
-    x = (float)new_x;
-    y = (float)new_y;
+pair<float, float> Point::get_polar_coord(float c_x, float c_y) {
+    float r_y = y - c_y;
+    float r_x = x - c_x;
+    if (radius == -1) {
+        radius = sqrt(r_x * r_x + r_y * r_y);
+    }
+    return {radius, atan2(r_y, r_x)};
 }
 
-void Point::set_x(float new_x){
+pair<float, float> Point::get_change_polar_coord(float r, float angle, float c_x, float c_y) {
+    return {r * cos(angle) + c_x - x, r * sin(angle) + c_y - y};
+}
+
+void Point::set_polar_coord(float r, float angle, float c_x, float c_y) {
+    x = r * cos(angle) + c_x;
+    y = r * sin(angle) + c_y;
+}
+
+void Point::move_point(float add_x, float add_y) {
+    x += add_x;
+    y += add_y;
+}
+
+void Point::set_coord(float new_x, float new_y) {
     x = new_x;
-}
-
-void Point::set_y(float new_y){
     y = new_y;
 }
 
-void Point::set_point(float new_x, float new_y){
-        x = new_x;
-        y = new_y;
+Point::Point() {
+    x = 0;
+    y = 0;
 }
+
+Point::Point(int my_canvas, int s_x, int s_y) {
+    canvas = my_canvas;
+    x = s_x;
+    y = s_y;
+}
+
+Point::Point(const Point &copy) {
+    canvas = copy.get_canvas();
+    x = copy.get_raw_x();
+    y = copy.get_raw_y();
+}
+
+tuple <shared_ptr<Point>, shared_ptr<Point>, shared_ptr<Point>, shared_ptr<Point>>
+Point::get_extreme_points(tuple <shared_ptr<Point>, shared_ptr<Point>, shared_ptr<Point>, shared_ptr<Point>> data) {
+    auto[max_x, max_y, min_x, min_y] = data;
+    if (max_x->get_raw_x() <= x) max_x = make_shared<Point>(*this);
+    if (max_y->get_raw_y() <= y) max_y = make_shared<Point>(*this);
+    if (min_x->get_raw_x() >= x) min_x = make_shared<Point>(*this);
+    if (min_y->get_raw_y() >= y) min_y = make_shared<Point>(*this);
+    return {max_x, max_y, min_x, min_y};
+}
+
+void Point::draw() {
+    cout << "Draw point";
+    cout << get_x();
+    cout << ", ";
+    cout << get_y() << endl;
+//            draw_point_python(canvas, x, y);
+}
+
+void Point::draw_line(Point &point) {
+    new_draw_line_python(canvas, x, y, point.get_raw_x(), point.get_raw_y());
+//            draw_line_python(canvas, x, y, point.get_raw_x(), point.get_raw_y());
+}
+
